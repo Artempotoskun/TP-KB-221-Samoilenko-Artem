46d65f7843721facee25eab14ab39ce37653dd58
Artempotoskun <250105as@gmail.com>
2023-12-02 21:06:55 +0200

    test laba3

--- lab_03/test_lab3.py
+++ lab_03/test_lab3.py
@@ -1,41 +1,28 @@
-def test_get_student(test_list):
-    student_list = StudentList()
+from lab3 import StudentList, Student
+import unittest
 
-    # Додаємо студентів до списку
-    for data in test_list:
-        student_list.add_student(Student(**data))
+class TestStudentList(unittest.TestCase):
+    def setUp(self):
+        self.student_list = StudentList()
 
-    # Тест на отримання студента за іменем
-    retrieved_student = student_list.get_student('Bob')
-    assert retrieved_student is not None
-    assert retrieved_student.name == 'Bob'
-    assert retrieved_student.phone == '1112233'
+    def test_add_student(self):
+        student = Student('John', '1234567890')
+        self.student_list.add_student(student)
+        self.assertEqual(self.student_list.students_list[0], student)
 
-    # Тест на випадок, коли студент не існує
-    non_existent_student = student_list.get_student('NonExistent')
-    assert non_existent_student is None
+    def test_delete_student(self):
+        student = Student('John', '1234567890')
+        self.student_list.add_student(student)
+        self.student_list.delete_student('John')
+        self.assertEqual(len(self.student_list.students_list), 0)
 
-def test_list_students(test_list, capsys):
-    student_list = StudentList()
+    def test_update_student(self):
+        student = Student('John', '1234567890')
+        self.student_list.add_student(student)
+        updated_student = Student('John', '0987654321')
+        self.student_list.update_student(updated_student)
+        self.assertEqual(self.student_list.students_list[0].phone, '0987654321')
 
-    # Додаємо студентів до списку
-    for data in test_list:
-        student_list.add_student(Student(**data))
+if __name__ == '__main__':
+    unittest.main(exit=False)
 
-    # Тест на вивід списку студентів
-    student_list.list_students()
-    captured = capsys.readouterr()
-    assert 'Bob' in captured.out
-    assert 'Dilan' in captured.out
-    assert 'Zak' in captured.out
-    assert 'Mike' not in captured.out  # Новий студент не виводиться
-
-def test_invalid_phone_input(test_list):
-    with patch('builtins.input', side_effect=["John", "invalid_phone"]):
-        student_list = StudentList()
-
-        # Додаємо студента до списку через некоректний номер телефону
-        student_list.add_student(Student(**Utils.get_new_params(('name', 'phone'))))
-
-    # Студент не додався через некоректний номер телефону
-    assert len(student_list.students_list) == 0
